016a693cd9012bd76938f4ce6383b120080b1743
Tiln <tiln141@gmail.om>
2017-10-17 08:39:52 -0600

    added resetboard and "black's second move can't be in a the middle 25" to Board

--- src/main/java/group/csc280/pente/model/Board.java
+++ src/main/java/group/csc280/pente/model/Board.java
@@ -1,29 +1,9 @@
 package group.csc280.pente.model;
 
-
-
 public class Board {
-	public enum Direction {
-		left(-1, 0),
-		right(1,0),
-		down(0, -1),
-		up(0, 1),
-		upright(1, 1),
-		upleft(-1, 1),
-		downleft(-1, -1),
-		downright(1, -1);
-		
-		public int dx;
-		public int dy;
-		
-		Direction (int dx, int dy) {
-			this.dx = dx;
-			this.dy = dy;
-		}
-	}
-	
 	private char[][] board;
-
+	
+//Initializes the board state
 	public Board() {
 		board = new char[19][19];
 		for (int i = 0; i < board.length; i++) {
@@ -33,40 +13,61 @@ public class Board {
 		}
 		board[9][9] = 'B';
 	}
-
+	
+//Returns the board's current state
 	public char[][] getBoard(){
 		return board;
 	}
 	
-	public void printBoard() {
+	//resets the board to its initial state
+	public boolean resetBoard() {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				board[i][j] = ' ';
+			}
+		}
+		board[9][9] = 'B';
+		return true;
+	}
+	
+//Prints the board to the console
+	public boolean printBoard() {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				System.out.print("[" + board[i][j] + "]");
 			}
 			System.out.println();
 		}
+		return true;
 	}
-
-	public boolean makeMove(char turn, int x, int y) {
-		if (isValidMove(x, y)) {
+	
+//makes a move after validating the move
+	public boolean makeMove(char turn, int x, int y, boolean isBlacksSecondMove) {
+		if (isValidMove(x, y, isBlacksSecondMove)) {
 			board[x][y] = turn;
 			return true;
 		}
 		return false;
 	}
-
-	public boolean isValidMove(int x, int y) {
-		if (board[x][y] == ' ') {
+	
+//checks if the attempted move is a valid move
+	public boolean isValidMove(int x, int y, boolean isBlacksSecondMove) {
+		if (board[x][y] == ' '&&!isBlacksSecondMove) {
+			return true;
+		}
+		else if (isBlacksSecondMove&&board[x][y] == ' '&&(x<7||x>11)&&(y<7||y>11)) {
 			return true;
 		}
 		return false;
 	}
-
+	
+//Deletes the two pieces that need capturing
 	private void capture(Direction direction, int x, int y) {
 		board[x+direction.dx][y+direction.dy] = ' ';
 		board[x+(direction.dx*2)][y+(direction.dy*2)] = ' ';
 	}
 
+//Checks if the placed piece captures anything and calls capture to capture the pieces
 	public boolean checkCapture(char turn, char notTurn, int x, int y) {
 		int right = 0;
 		int up = 0;
@@ -192,6 +193,7 @@ public class Board {
 		return found;
 	}
 
+//checks to see if the piece just placed causes that player to win
 	public boolean isWinner(char turn, int x, int y) {
 		int horiz = 0;
 		int vert = 0;
