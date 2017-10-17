bf095c9867b02a316124df93d3af970135ad312e
Parker Dunbar <PDunbar@student.neumont.edu>
2017-10-17 08:24:07 -0600

    Working GUI

--- src/main/java/group/csc280/pente/model/Board.java
+++ src/main/java/group/csc280/pente/model/Board.java
@@ -1,27 +1,6 @@
 package group.csc280.pente.model;
 
-
-
 public class Board {
-	public enum Direction {
-		left(-1, 0),
-		right(1,0),
-		down(0, -1),
-		up(0, 1),
-		upright(1, 1),
-		upleft(-1, 1),
-		downleft(-1, -1),
-		downright(1, -1);
-		
-		public int dx;
-		public int dy;
-		
-		Direction (int dx, int dy) {
-			this.dx = dx;
-			this.dy = dy;
-		}
-	}
-	
 	private char[][] board;
 
 	public Board() {
